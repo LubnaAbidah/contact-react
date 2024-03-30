24f56fa508076bb49b8edc41a91f92e2b88fa103
Lubna Abidah <lubna16abidah@gmail.com>
2024-03-31 00:27:21 +0700

    move styling to index.jsx from index.html

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import ContactApp from "./ContactApp";
 
 /*const element = <h1>Hello, world!</h1>; */
 
+//styling
+import './style.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<ContactApp />);
